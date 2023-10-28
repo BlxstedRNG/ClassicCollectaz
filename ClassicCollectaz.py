08e8d287be94bc3244c8234a03a0fb7632b57a2f
max <148343900+BlxstedRNG@users.noreply.github.com>
2023-10-28 20:27:59 +0100

    Add files via upload
    Support for up to SW0401
    names or ID's

---
+++ ClassicCollectaz.py
@@ -0,0 +1,815 @@
+names = """
+luke skywalker
+princess leia
+han solo
+chewbacca
+darth vader
+stormtrooper
+boba fett
+obi-wan kenobi
+yoda
+r2-d2
+c-3po
+emperor palpatine
+imperial guard
+anakin skywalker
+padmé amidala
+clone trooper
+general grievous
+count dooku
+mace windu
+ahsoka tano
+darth maul
+jango fett
+lando calrissian
+jabba the hutt
+gamorrean guard
+wicket w. warrick
+ewok warrior
+admiral ackbar
+mon mothma
+nien nunb
+bib fortuna
+max rebo
+sy snootles
+ig-88
+dengar
+bossk
+4-lom
+zuckuss
+e-3po
+bespin guard
+lobot
+leia (bespin)
+luke (bespin)
+han (bespin)
+cloud city guard
+ugnaught
+rebel pilot
+hoth han solo
+tauntaun
+wampa
+snowtrooper
+at-at driver
+at-at commander
+at-at pilot
+general veers
+rebel officer
+hoth rebel trooper
+echo base trooper
+wampa cave luke
+wampa cave han
+wedge antilles
+biggs darklighter
+snowspeeder pilot
+rebel technician
+hoth trooper
+luke (hoth)
+han (hoth)
+leia (hoth)
+r3-a2
+luke (yavin)
+han (yavin)
+leia (yavin)
+rebel trooper
+imperial navy officer
+imperial ground crew
+at-st driver
+imperial shuttle pilot
+hoth rebel officer
+hoth rebel trooper (open parka)
+han solo (hoth)
+luke (hoth, open parka)
+rebel pilot (hoth)
+luke (dagobah)
+yoda (dagobah)
+r2-d2 (dagobah)
+mace windu (episode ii)
+obi-wan (episode ii)
+lama su
+taun we
+barriss offee
+clone pilot
+luke (tan hair)
+han (tan legs)
+leia (tan hair)
+jango fett (new torso)
+clone trooper (phase ii)
+obi-wan (episode iii)
+anakin (episode iii)
+aayla secura
+commander bly
+clone trooper (episode iii)
+clone trooper (episode iii, pilot)
+clone trooper (episode iii, walker pilot)
+clone trooper (episode iii, gunner)
+clone trooper (episode iii, bomb squad)
+clone trooper (episode iii, helmet antenna)
+clone trooper (episode iii, pilot, helmet antenna)
+clone trooper (episode iii, walker pilot, helmet antenna)
+clone trooper (episode iii, gunner, helmet antenna)
+clone trooper (episode iii, bomb squad, helmet antenna)
+hoth han solo (hood)
+hoth han solo (goggles)
+hoth han solo (hood, goggles)
+hoth rebel trooper (dark tan hood)
+hoth rebel trooper (dark tan hood, scarf)
+rebel technician (dark tan hood)
+rebel technician (dark tan hood, scarf)
+hoth rebel officer (dark tan hood)
+hoth rebel officer (dark tan hood, scarf)
+luke skywalker (y-wing pilot)
+han solo (y-wing pilot)
+luke skywalker (x-wing pilot)
+biggs darklighter (x-wing pilot)
+jek porkins
+r5-d8
+astromech droid (dark red, gray head)
+rebel pilot (helmet, goggles)
+rebel pilot (helmet, scarf)
+rebel pilot (helmet, goggles, scarf)
+imperial officer (dark tan hat, black visor)
+imperial officer (dark tan hat, no visor)
+at-st driver (helmet)
+at-st driver (helmet, scarf)
+at-st driver (helmet, goggles)
+at-st driver (helmet, goggles, scarf)
+imperial shuttle pilot (dark gray)
+imperial shuttle pilot (light gray)
+luke skywalker (hoth)
+han solo (hoth, open parka)
+han solo (hoth, goggles)
+leia (hoth, hood)
+leia (hoth, open parka)
+leia (hoth, hood, goggles)
+rebel trooper (hood)
+rebel trooper (hood, scarf)
+rebel trooper (hood, goggles)
+rebel trooper (hood, goggles, scarf)
+imperial navy officer (dark gray hat)
+imperial navy officer (dark gray hat, black visor)
+imperial navy officer (dark gray hat, no visor)
+imperial ground crew (dark gray hat)
+imperial ground crew (dark gray hat, goggles)
+imperial ground crew (dark gray hat, scarf)
+imperial ground crew (dark gray hat, goggles, scarf)
+imperial ground crew (light gray hat)
+imperial ground crew (light gray hat, goggles)
+imperial ground crew (light gray hat, scarf)
+imperial ground crew (light gray hat, goggles, scarf)
+imperial ground crew (black hat)
+imperial ground crew (black hat, goggles)
+imperial ground crew (black hat, scarf)
+imperial ground crew (black hat, goggles, scarf)
+rebel pilot (dark tan helmet, goggles)
+rebel pilot (dark tan helmet, scarf)
+rebel pilot (dark tan helmet, goggles, scarf)
+rebel pilot (light gray helmet, goggles)
+rebel pilot (light gray helmet, scarf)
+rebel pilot (light gray helmet, goggles, scarf)
+rebel pilot (black helmet, goggles)
+rebel pilot (black helmet, scarf)
+rebel pilot (black helmet, goggles, scarf)
+rebel pilot (dark red helmet, goggles)
+rebel pilot (dark red helmet, scarf)
+rebel pilot (dark red helmet, goggles, scarf)
+snowtrooper (dark tan hood)
+snowtrooper (dark tan hood, goggles)
+snowtrooper (dark tan hood, scarf)
+snowtrooper (dark tan hood, goggles, scarf)
+imperial officer (dark gray hat, goggles)
+imperial officer (dark gray hat, scarf)
+imperial officer (dark gray hat, goggles, scarf)
+imperial officer (light gray hat, goggles)
+imperial officer (light gray hat, scarf)
+imperial officer (light gray hat, goggles, scarf)
+imperial officer (black hat, goggles)
+imperial officer (black hat, scarf)
+imperial officer (black hat, goggles, scarf)
+imperial officer (dark red hat, goggles)
+imperial officer (dark red hat, scarf)
+imperial officer (dark red hat, goggles, scarf)
+imperial officer (black helmet)
+imperial officer (black helmet, goggles)
+imperial officer (black helmet, scarf)
+imperial officer (black helmet, goggles, scarf)
+imperial officer (dark red helmet)
+imperial officer (dark red helmet, goggles)
+imperial officer (dark red helmet, scarf)
+imperial officer (dark red helmet, goggles, scarf)
+imperial officer (dark tan helmet)
+imperial officer (dark tan helmet, goggles)
+imperial officer (dark tan helmet, scarf)
+imperial officer (dark tan helmet, goggles, scarf)
+hoth rebel officer (dark tan hat, goggles)
+hoth rebel officer (dark tan hat, scarf)
+hoth rebel officer (dark tan hat, goggles, scarf)
+hoth rebel officer (light gray hat, goggles)
+hoth rebel officer (light gray hat, scarf)
+hoth rebel officer (light gray hat, goggles, scarf)
+hoth rebel officer (black hat, goggles)
+hoth rebel officer (black hat, scarf)
+hoth rebel officer (black hat, goggles, scarf)
+hoth rebel officer (dark red hat, goggles)
+hoth rebel officer (dark red hat, scarf)
+hoth rebel officer (dark red hat, goggles, scarf)
+hoth rebel officer (black helmet)
+hoth rebel officer (black helmet, goggles)
+hoth rebel officer (black helmet, scarf)
+hoth rebel officer (black helmet, goggles, scarf)
+hoth rebel officer (dark red helmet)
+hoth rebel officer (dark red helmet, goggles)
+hoth rebel officer (dark red helmet, scarf)
+hoth rebel officer (dark red helmet, goggles, scarf)
+general veers (imperial uniform)
+general veers (at-at pilot)
+rebel pilot (hoth, white helmet)
+rebel pilot (hoth, gray helmet)
+rebel pilot (hoth, white helmet, goggles)
+rebel pilot (hoth, gray helmet, goggles)
+rebel pilot (hoth, white helmet, scarf)
+rebel pilot (hoth, gray helmet, scarf)
+rebel pilot (hoth, white helmet, goggles, scarf)
+rebel pilot (hoth, gray helmet, goggles, scarf)
+snowtrooper (dark tan hood, mask)
+snowtrooper (dark tan hood, goggles, mask)
+imperial officer (dark gray hat, goggles, scarf)
+imperial officer (light gray hat, goggles, scarf)
+imperial officer (black hat, goggles, scarf)
+imperial officer (dark red hat, goggles, scarf)
+imperial officer (black helmet, goggles, scarf)
+imperial officer (dark red helmet, goggles, scarf)
+imperial officer (dark tan helmet, goggles, scarf)
+hoth rebel officer (dark tan hat, goggles, scarf)
+hoth rebel officer (light gray hat, goggles, scarf)
+hoth rebel officer (black hat, goggles, scarf)
+hoth rebel officer (dark red hat, goggles, scarf)
+hoth rebel officer (black helmet, goggles, scarf)
+hoth rebel officer (dark red helmet, goggles, scarf)
+hoth rebel officer (dark tan helmet, goggles, scarf)
+rebel pilot (hoth, white helmet, goggles, scarf)
+rebel pilot (hoth, gray helmet, goggles, scarf)
+wampa cave luke (bloody)
+wampa cave han (bloody)
+wedge antilles (x-wing pilot)
+biggs darklighter (x-wing pilot, helmet)
+jek porkins (x-wing pilot)
+r5-d8 (x-wing pilot)
+astromech droid (dark red, gray head, x-wing pilot)
+rebel pilot (helmet, goggles, scarf, x-wing pilot)
+imperial officer (dark gray hat, black visor, x-wing pilot)
+imperial officer (dark gray hat, no visor, x-wing pilot)
+at-st driver (helmet, x-wing pilot)
+at-st driver (helmet, scarf, x-wing pilot)
+at-st driver (helmet, goggles, x-wing pilot)
+at-st driver (helmet, goggles, scarf, x-wing pilot)
+imperial shuttle pilot (dark gray, x-wing pilot)
+imperial shuttle pilot (light gray, x-wing pilot)
+luke skywalker (hoth, x-wing pilot)
+han solo (hoth, open parka, x-wing pilot)
+han solo (hoth, goggles, x-wing pilot)
+leia (hoth, hood, x-wing pilot)
+leia (hoth, open parka, x-wing pilot)
+leia (hoth, hood, goggles, x-wing pilot)
+rebel trooper (hood, x-wing pilot)
+rebel trooper (hood, scarf, x-wing pilot)
+rebel trooper (hood, goggles, x-wing pilot)
+rebel trooper (hood, goggles, scarf, x-wing pilot)
+imperial navy officer (dark gray hat, x-wing pilot)
+imperial navy officer (dark gray hat, black visor, x-wing pilot)
+imperial navy officer (dark gray hat, no visor, x-wing pilot)
+imperial navy officer (dark gray hat, goggles, x-wing pilot)
+imperial navy officer (dark gray hat, goggles, scarf, x-wing pilot)
+imperial navy officer (light gray hat, x-wing pilot)
+imperial navy officer (light gray hat, black visor, x-wing pilot)
+imperial navy officer (light gray hat, no visor, x-wing pilot)
+imperial navy officer (light gray hat, goggles, x-wing pilot)
+imperial navy officer (light gray hat, goggles, scarf, x-wing pilot)
+imperial navy officer (black hat, x-wing pilot)
+imperial navy officer (black hat, black visor, x-wing pilot)
+imperial navy officer (black hat, no visor, x-wing pilot)
+imperial navy officer (black hat, goggles, x-wing pilot)
+imperial navy officer (black hat, goggles, scarf, x-wing pilot)
+imperial navy officer (dark red hat, x-wing pilot)
+imperial navy officer (dark red hat, black visor, x-wing pilot)
+imperial navy officer (dark red hat, no visor, x-wing pilot)
+imperial navy officer (dark red hat, goggles, x-wing pilot)
+imperial navy officer (dark red hat, goggles, scarf, x-wing pilot)
+imperial navy officer (black helmet, x-wing pilot)
+imperial navy officer (black helmet, black visor, x-wing pilot)
+imperial navy officer (black helmet, no visor, x-wing pilot)
+imperial navy officer (black helmet, goggles, x-wing pilot)
+imperial navy officer (black helmet, goggles, scarf, x-wing pilot)
+b-wing pilot (helmet, x-wing pilot)
+b-wing pilot (helmet, scarf, x-wing pilot)
+b-wing pilot (helmet, goggles, x-wing pilot)
+b-wing pilot (helmet, goggles, scarf, x-wing pilot)
+luke skywalker (dagobah, pilot suit)
+luke skywalker (dagobah, helmet)
+luke skywalker (dagobah, helmet, backpack)
+luke skywalker (dagobah, helmet, backpack, scarf)
+luke skywalker (dagobah, helmet, backpack, scarf, flashlight)
+luke skywalker (dagobah, hood)
+luke skywalker (dagobah, hood, backpack)
+luke skywalker (dagobah, hood, backpack, scarf)
+luke skywalker (dagobah, hood, backpack, scarf, flashlight)
+luke skywalker (dagobah, hood, backpack, scarf, flashlight, rope)
+luke skywalker (dagobah, pilot suit, helmet)
+luke skywalker (dagobah, pilot suit, helmet, backpack)
+luke skywalker (dagobah, pilot suit, helmet, backpack, scarf)
+luke skywalker (dagobah, pilot suit, helmet, backpack, scarf, flashlight)
+luke skywalker (dagobah, pilot suit, helmet, backpack, scarf, flashlight, rope)
+yoda
+at-at driver (dark gray helmet)
+at-at driver (dark gray helmet, goggles)
+at-at driver (dark gray helmet, scarf)
+at-at driver (dark gray helmet, goggles, scarf)
+at-at driver (light gray helmet)
+at-at driver (light gray helmet, goggles)
+at-at driver (light gray helmet, scarf)
+at-at driver (light gray helmet, goggles, scarf)
+at-at driver (black helmet)
+at-at driver (black helmet, goggles)
+at-at driver (black helmet, scarf)
+at-at driver (black helmet, goggles, scarf)
+at-at driver (dark red helmet)
+at-at driver (dark red helmet, goggles)
+at-at driver (dark red helmet, scarf)
+at-at driver (dark red helmet, goggles, scarf)
+hoth rebel soldier (hood, goggles)
+hoth rebel soldier (hood, scarf, goggles)
+hoth rebel soldier (hood, backpack, goggles)
+hoth rebel soldier (hood, backpack, scarf, goggles)
+rebel pilot (hoth, dark tan helmet, goggles, scarf)
+rebel pilot (hoth, dark tan helmet, goggles, backpack, scarf)
+rebel pilot (hoth, dark tan helmet, goggles, backpack, scarf, flashlight)
+imperial navy officer (dark gray hat, goggles, x-wing pilot)
+imperial navy officer (light gray hat, goggles, x-wing pilot)
+imperial navy officer (black hat, goggles, x-wing pilot)
+imperial navy officer (dark red hat, goggles, x-wing pilot)
+imperial navy officer (black helmet, goggles, x-wing pilot)
+imperial navy officer (dark red helmet, goggles, x-wing pilot)
+imperial navy officer (dark tan helmet, goggles, x-wing pilot)
+hoth rebel officer (dark tan hat, goggles, x-wing pilot)
+hoth rebel officer (light gray hat, goggles, x-wing pilot)
+hoth rebel officer (black hat, goggles, x-wing pilot)
+hoth rebel officer (dark red hat, goggles, x-wing pilot)
+hoth rebel officer (black helmet, goggles, x-wing pilot)
+hoth rebel officer (dark red helmet, goggles, x-wing pilot)
+hoth rebel officer (dark tan helmet, goggles, x-wing pilot)
+rebel pilot (hoth, white helmet, goggles, scarf, x-wing pilot)
+rebel pilot (hoth, gray helmet, goggles, scarf, x-wing pilot)
+rebel pilot (hoth, dark tan helmet, goggles, scarf, x-wing pilot)
+rebel pilot (hoth, dark tan helmet, goggles, backpack, scarf, x-wing pilot)
+rebel pilot (hoth, dark tan helmet, goggles, backpack, scarf, flashlight, x-wing pilot)
+snowtrooper (dark tan hood, mask, scarf)
+snowtrooper (dark tan hood, goggles, mask, scarf)
+imperial officer (dark gray hat, goggles, scarf, x-wing pilot)
+imperial officer (light gray hat, goggles, scarf, x-wing pilot)
+imperial officer (black hat, goggles, scarf, x-wing pilot)
+imperial officer (dark red hat, goggles, scarf, x-wing pilot)
+imperial officer (black helmet, goggles, scarf, x-wing pilot)
+imperial officer (dark red helmet, goggles, scarf, x-wing pilot)
+imperial officer (dark tan helmet, goggles, scarf, x-wing pilot)
+hoth rebel officer (dark tan hat, goggles, scarf, x-wing pilot)
+hoth rebel officer (light gray hat, goggles, scarf, x-wing pilot)
+hoth rebel officer (black hat, goggles, scarf, x-wing pilot)
+hoth rebel officer (dark red hat, goggles, scarf, x-wing pilot)
+hoth rebel officer (black helmet, goggles, scarf, x-wing pilot)
+hoth rebel officer (dark red helmet, goggles, scarf, x-wing pilot)
+hoth rebel officer (dark tan helmet, goggles, scarf, x-wing pilot)
+rebel pilot (hoth, white helmet, goggles, scarf, flashlight, x-wing pilot)
+rebel pilot (hoth, gray helmet, goggles, scarf, flashlight, x-wing pilot)
+rebel pilot (hoth, dark tan helmet, goggles, scarf, flashlight, x-wing pilot)
+rebel pilot (hoth, dark tan helmet, goggles, backpack, scarf, flashlight, x-wing pilot)
+rebel pilot (hoth, dark tan helmet, goggles, backpack, scarf, flashlight, rope, x-wing pilot)
+imperial navy officer (dark gray hat, goggles, x-wing pilot, backpack)
+imperial navy officer (light gray hat, goggles, x-wing pilot, backpack)
+imperial navy officer (black hat, goggles, x-wing pilot, backpack)
+imperial navy officer (dark red hat, goggles, x-wing pilot, backpack)
+imperial navy officer (black helmet, goggles, x-wing pilot, backpack)
+imperial navy officer (dark red helmet, goggles, x-wing pilot, backpack)
+imperial navy officer (dark tan helmet, goggles, x-wing pilot, backpack)
+hoth rebel officer (dark tan hat, goggles, x-wing pilot, backpack)
+hoth rebel officer (light gray hat, goggles, x-wing pilot, backpack)
+hoth rebel officer (black hat, goggles, x-wing pilot, backpack)
+hoth rebel officer (dark red hat, goggles, x-wing pilot, backpack)
+hoth rebel officer (black helmet, goggles, x-wing pilot, backpack)
+hoth rebel officer (dark red helmet, goggles, x-wing pilot, backpack)
+hoth rebel officer (dark tan helmet, goggles, x-wing pilot, backpack)
+rebel pilot (hoth, white helmet, goggles, scarf, x-wing pilot, backpack)
+rebel pilot (hoth, gray helmet, goggles, scarf, x-wing pilot, backpack)
+"""
+ids = """
+sw0001
+sw0002
+sw0003
+sw0004
+sw0005
+sw0006
+sw0007
+sw0008
+sw0009
+sw0010
+sw0011
+sw0012
+sw0013
+sw0014
+sw0015
+sw0016
+sw0017
+sw0018
+sw0019
+sw0020
+sw0021
+sw0022
+sw0023
+sw0024
+sw0025
+sw0026
+sw0027
+sw0028
+sw0029
+sw0030
+sw0031
+sw0032
+sw0033
+sw0034
+sw0035
+sw0036
+sw0037
+sw0038
+sw0039
+sw0040
+sw0041
+sw0042
+sw0043
+sw0044
+sw0045
+sw0046
+sw0047
+sw0048
+sw0049
+sw0050
+sw0051
+sw0052
+sw0053
+sw0054
+sw0055
+sw0056
+sw0057
+sw0058
+sw0059
+sw0060
+sw0061
+sw0062
+sw0063
+sw0064
+sw0065
+sw0066
+sw0067
+sw0068
+sw0069
+sw0070
+sw0071
+sw0072
+sw0073
+sw0074
+sw0075
+sw0076
+sw0077
+sw0078
+sw0079
+sw0080
+sw0081
+sw0082
+sw0083
+sw0084
+sw0085
+sw0086
+sw0087
+sw0088
+sw0089
+sw0090
+sw0091
+sw0092
+sw0093
+sw0094
+sw0095
+sw0096
+sw0097
+sw0098
+sw0099
+sw0100
+sw0101
+sw0102
+sw0103
+sw0104
+sw0105
+sw0106
+sw0107
+sw0108
+sw0109
+sw0110
+sw0111
+sw0112
+sw0113
+sw0114
+sw0115
+sw0116
+sw0117
+sw0118
+sw0119
+sw0120
+sw0121
+sw0122
+sw0123
+sw0124
+sw0125
+sw0126
+sw0127
+sw0128
+sw0129
+sw0130
+sw0131
+sw0132
+sw0133
+sw0134
+sw0135
+sw0136
+sw0137
+sw0138
+sw0139
+sw0140
+sw0141
+sw0142
+sw0143
+sw0144
+sw0145
+sw0146
+sw0147
+sw0148
+sw0149
+sw0150
+sw0151
+sw0152
+sw0153
+sw0154
+sw0155
+sw0156
+sw0157
+sw0158
+sw0159
+sw0160
+sw0161
+sw0162
+sw0163
+sw0164
+sw0165
+sw0166
+sw0167
+sw0168
+sw0169
+sw0170
+sw0171
+sw0172
+sw0173
+sw0174
+sw0175
+sw0176
+sw0177
+sw0178
+sw0179
+sw0180
+sw0181
+sw0182
+sw0183
+sw0184
+sw0185
+sw0186
+sw0187
+sw0188
+sw0189
+sw0190
+sw0191
+sw0192
+sw0193
+sw0194
+sw0195
+sw0196
+sw0197
+sw0198
+sw0199
+sw0200
+sw0201
+sw0202
+sw0203
+sw0204
+sw0205
+sw0206
+sw0207
+sw0208
+sw0209
+sw0210
+sw0211
+sw0212
+sw0213
+sw0214
+sw0215
+sw0216
+sw0217
+sw0218
+sw0219
+sw0220
+sw0221
+sw0222
+sw0223
+sw0224
+sw0225
+sw0226
+sw0227
+sw0228
+sw0229
+sw0230
+sw0231
+sw0232
+sw0233
+sw0234
+sw0235
+sw0236
+sw0237
+sw0238
+sw0239
+sw0240
+sw0241
+sw0242
+sw0243
+sw0244
+sw0245
+sw0246
+sw0247
+sw0248
+sw0249
+sw0250
+sw0251
+sw0252
+sw0253
+sw0254
+sw0255
+sw0256
+sw0257
+sw0258
+sw0259
+sw0260
+sw0261
+sw0262
+sw0263
+sw0264
+sw0265
+sw0266
+sw0267
+sw0268
+sw0269
+sw0270
+sw0271
+sw0272
+sw0273
+sw0274
+sw0275
+sw0276
+sw0277
+sw0278
+sw0279
+sw0280
+sw0281
+sw0282
+sw0283
+sw0284
+sw0285
+sw0286
+sw0287
+sw0288
+sw0289
+sw0290
+sw0291
+sw0292
+sw0293
+sw0294
+sw0295
+sw0296
+sw0297
+sw0298
+sw0299
+sw0300
+sw0301
+sw0302
+sw0303
+sw0304
+sw0305
+sw0306
+sw0307
+sw0308
+sw0309
+sw0310
+sw0311
+sw0312
+sw0313
+sw0314
+sw0315
+sw0316
+sw0317
+sw0318
+sw0319
+sw0320
+sw0321
+sw0322
+sw0323
+sw0324
+sw0325
+sw0326
+sw0327
+sw0328
+sw0329
+sw0330
+sw0331
+sw0332
+sw0333
+sw0334
+sw0335
+sw0336
+sw0337
+sw0338
+sw0339
+sw0340
+sw0341
+sw0342
+sw0343
+sw0344
+sw0345
+sw0346
+sw0347
+sw0348
+sw0349
+sw0350
+sw0351
+sw0352
+sw0353
+sw0354
+sw0355
+sw0356
+sw0357
+sw0358
+sw0359
+sw0360
+sw0361
+sw0362
+sw0363
+sw0364
+sw0365
+sw0366
+sw0367
+sw0368
+sw0369
+sw0370
+sw0371
+sw0372
+sw0373
+sw0374
+sw0375
+sw0376
+sw0377
+sw0378
+sw0379
+sw0380
+sw0381
+sw0382
+sw0383
+sw0384
+sw0385
+sw0386
+sw0387
+sw0388
+sw0389
+sw0390
+sw0391
+sw0392
+sw0393
+sw0394
+sw0395
+sw0396
+sw0397
+sw0398
+sw0399
+sw0400
+sw0401
+"""
+while True:
+    either = (input("Enter a SW Mini-Figure name or sw code "))
+    if either in ids:
+        print(either, "Found by ID")
+    
+    if either in names:
+        print(either, "Found by name")
+
+    else:
+        print("Error")
